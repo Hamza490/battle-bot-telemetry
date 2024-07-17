f28157c2fcef277446edcff4833b0e8b0020acf6
Hamza <muhammadmhamza35@gmail.com>
2024-07-16 20:11:56 -0400

    reorganized pyqt5 tutorial practice

--- pyqt5-tutorial/app.py
+++ pyqt5-tutorial/app.py
@@ -1,8 +1,29 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+from PyQt5.QtCore import QSize, Qt
 import sys
+'''
+This code was made to practice creating individual window:
+window=QWidget()
+window2=QPushButton("Push my buttons :p")
+window3=QMainWindow()
 
+#window.show()
+#window2.show()
+window3.show()
+'''
+
+class MainWindow(QMainWindow):
+  def __init__(self):
+    super().__init__()
+
+    self.setWindowTitle("Battle-Bot Telemetry")
+    button = QPushButton("Port")
+
+    self.setCentralWidget(button)
+
+    self.setFixedSize(QSize(1200,800))
 app = QApplication(sys.argv)
 
-window=QWidget()
+window = MainWindow()
 window.show()
 app.exec()
